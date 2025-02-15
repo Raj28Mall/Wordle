c31fb4216a122adfca322e6e8104576304ccb6f2
Raj Mall <rajmall.0206@gmail.com>
2025-02-16 01:43:07 +0530

    Basic dialog box setup, styling required

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,17 @@
 <script>
-  let name = $state("Raj");
+  import { onMount } from "svelte";
+  let name = $state("Chinaaaaaaaaaaaaaaaaaaaaaa");
+  let dialog;
   import Navbar from './Navbar/Navbar.svelte';
+  
+  onMount(() => {
+    dialog.showModal();
+  });
+
+  const closeDialog=()=>{
+    dialog.close();
+  }
+
 </script>
 
 <main class="bg-[#121213] min-h-screen">
@@ -8,3 +19,12 @@
     <Navbar name={name}/>
   </header>
 </main>
+
+
+<dialog bind:this={dialog}>
+  <form method="dialog">
+    <label for="name">Name:</label>
+    <input type="text" id="name" bind:value={name} required placeholder="Enter your name">
+    <button type="submit" onclick={closeDialog}>Submit</button>
+  </form>
+</dialog>
--- frontend/src/Navbar/Navbar.svelte
+++ frontend/src/Navbar/Navbar.svelte
@@ -6,6 +6,6 @@
         <div class="header text-white relative left-[47.5%] text-2xl">Wordle</div>
         <div class="welcome text-white relative left-[83%] flex items-center">
             <img src="https://cdn-icons-png.flaticon.com/512/847/847969.png" alt="Account" class="w-[30px] h-[30px]"/>
-            <div class="px-3 w-fit truncate">{name}</div>
+            <div class="px-3 w-[9vw] truncate">{name}</div>
         </div>
     </div>
