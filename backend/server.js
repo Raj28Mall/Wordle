48a476333c1fbaec09f13382c2ee34da13e62f99
Raj Mall <rajmall.0206@gmail.com>
2025-02-25 12:31:03 +0530

    Backend layout done, API requests left

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,45 @@
 const express=require('express');
+const mysql=require('mysql2');
 const app=express();
+const db = mysql.createConnection({
+    host: 'localhost',    
+    user: 'root',    
+    password: 'Jee2024!raj',  
+    database: 'test_words'
+});
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.status(200);
-    res.render('index');
+let guessed_word='';
+async function getWord(){
+    guessed_word='GHOST';
+}
+
+async function checkValidity(word){
+    let valid=false;
+    db.query(`SELECT EXISTS(SELECT 1 FROM words WHERE word = ?) AS word_exists`, [word], 
+      (err, results) => {
+        if (err) {
+            console.error("Database Error: ", err);
+        }
+        
+        if(results[0].word_exists){
+            valid=true;
+        }
+    });
+    return valid;
+}
+
+async function handleValidity(){
+    await getWord();
+    let valid=await checkValidity(guessed_word);
+    if(valid){
+        //send response
     }
-);
+    else{
+        //send response
+    }
+}
 
 const PORT=3000;
 app.listen(PORT);
